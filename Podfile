b3e056bfbebb94bee4bb8302098a3fc76844beb9
Benjamin Dodgson <benji@tomorrow.me>
2018-12-25 16:58:38 -0800

    adding twilio with ignore

--- Podfile
+++ Podfile
@@ -5,6 +5,8 @@ target 'Benji' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
   pod 'Parse'
+  pod 'TwilioChatClient'
+  pod 'TwilioAccessManager'
   # Pods for Benji
 
 end
--- Pods/Target Support Files/Pods-Benji/Pods-Benji-frameworks.sh
+++ Pods/Target Support Files/Pods-Benji/Pods-Benji-frameworks.sh
@@ -145,10 +145,16 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Bolts/Bolts.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Parse/Parse.framework"
+  install_framework "${PODS_ROOT}/TwilioAccessManager/TwilioAccessManager.framework"
+  install_framework "${PODS_ROOT}/TwilioChatClient/TwilioChatClient.framework"
+  install_dsym "${PODS_ROOT}/TwilioChatClient/TwilioChatClient.framework.dSYM"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Bolts/Bolts.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Parse/Parse.framework"
+  install_framework "${PODS_ROOT}/TwilioAccessManager/TwilioAccessManager.framework"
+  install_framework "${PODS_ROOT}/TwilioChatClient/TwilioChatClient.framework"
+  install_dsym "${PODS_ROOT}/TwilioChatClient/TwilioChatClient.framework.dSYM"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
