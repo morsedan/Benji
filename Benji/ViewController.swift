c16d10542103110e3a75d39b8968db423105d290
Benjamin Dodgson <benji@tomorrow.me>
2018-12-25 16:47:18 -0800

    Initial Commit

---
+++ Benji/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Benji
+//
+//  Created by Benji Dodgson on 12/25/18.
+//  Copyright © 2018 Benjamin Dodgson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
