61539504dd9b4f5a0e178db4fa6ef99e60684f9a
Benjamin Dodgson <benji@tomorrow.me>
2018-12-25 17:58:59 -0800

    Added Benji Theme

---
+++ Source/App/BenjiTheme.swift
@@ -0,0 +1,19 @@
+//
+//  BenjiTheme.swift
+//  Benji
+//
+//  Created by Benji Dodgson on 12/25/18.
+//  Copyright © 2018 Benjamin Dodgson. All rights reserved.
+//
+
+import Foundation
+
+struct BenjiTheme {
+    static let semibold: String = "SharpSansDisplayNo2-Semibold"
+    static let book: String = "SharpSansDisplayNo2-Book"
+    static let bold: String = "SharpSansDisplayNo2-Bold"
+
+    static let animationDuration: TimeInterval = 0.25
+
+    private init() {}
+}
