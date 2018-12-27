89c60c64ee0985cd134d9e8f4301f30ff1a77858
Benjamin Dodgson <benji@tomorrow.me>
2018-12-27 10:19:35 -0800

    Adding colors

--- Source/Models/View Models/Color.swift
+++ Source/Models/View Models/Color.swift
@@ -11,14 +11,9 @@ import Foundation
 enum Color: String, CaseIterable {
 
     case blue
-    case blue2
-    case blue3
-    case blue4
+    case green
+    case black
     case red
-    case yellow
-    case hotGreen
-    case orange
-    case ladderOrange
     case white
     case clear
     case halfWhite
@@ -26,29 +21,19 @@ enum Color: String, CaseIterable {
     var color: UIColor {
         switch self {
         case .blue:
-            return #colorLiteral(red: 0, green: 0.4509803922, blue: 0.7294117647, alpha: 1)
-        case .blue2:
-            return #colorLiteral(red: 0.337254902, green: 0.7490196078, blue: 0.9568627451, alpha: 1)
-        case .blue3:
-            return #colorLiteral(red: 0.6666666667, green: 0.8901960784, blue: 0.9764705882, alpha: 1)
-        case .blue4:
-            return #colorLiteral(red: 0.8980392157, green: 0.968627451, blue: 0.9960784314, alpha: 1)
+            return #colorLiteral(red: 0.3843137255, green: 0, blue: 0.9176470588, alpha: 1)
         case .red:
-            return #colorLiteral(red: 1, green: 0.1882352941, blue: 0.1137254902, alpha: 1)
-        case .yellow:
-            return #colorLiteral(red: 0.9843137255, green: 0.831372549, blue: 0.07450980392, alpha: 1)
-        case .hotGreen:
-            return #colorLiteral(red: 0.1490196078, green: 0.8941176471, blue: 0.4039215686, alpha: 1)
-        case .orange:
-            return #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
-        case .ladderOrange:
-            return #colorLiteral(red: 0.9960784314, green: 0.2784313725, blue: 0.2784313725, alpha: 1)
+            return #colorLiteral(red: 0.8666666667, green: 0.1725490196, blue: 0, alpha: 1)
+        case .green:
+            return #colorLiteral(red: 0, green: 0.7843137255, blue: 0.3254901961, alpha: 1)
         case .white:
             return #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         case .clear:
             return #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0)
         case .halfWhite:
             return #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.5)
+        case .black:
+            return #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         }
     }
 }
--- Source/UI/Screens/Center/CenterViewController.swift
+++ Source/UI/Screens/Center/CenterViewController.swift
@@ -9,7 +9,18 @@
 import Foundation
 
 class CenterViewController: FullScreenViewController {
-    
+
+    @IBOutlet weak var view1: View!
+    @IBOutlet weak var view2: View!
+    @IBOutlet weak var view3: View!
+
+    override func viewIsReadyForLayout() {
+        super.viewIsReadyForLayout()
+
+        self.view1.set(backgroundColor: .blue)
+        self.view2.set(backgroundColor: .green)
+        self.view3.set(backgroundColor: .red)
+    }
 }
 
 
