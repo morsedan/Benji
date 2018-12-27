1abbd54057a756ab52e618d228ca41a2b89ec0a4
Benjamin Dodgson <benji@tomorrow.me>
2018-12-27 09:59:38 -0800

    Added left, center, and right view controllers

--- Source/UI/Base Classes/ViewController.swift
+++ Source/UI/Base Classes/ViewController.swift
@@ -10,23 +10,7 @@ import UIKit
 
 class ViewController: UIViewController, Dismissable {
 
-    @IBOutlet weak var regular: UILabel!
-    @IBOutlet weak var demiBold: UILabel!
-    @IBOutlet weak var heavy: UILabel!
-    @IBOutlet weak var medium: UILabel!
-    @IBOutlet weak var ultraLight: UILabel!
-
     var didDismiss: (() -> Void)?
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        self.regular.font = UIFont(name: Theme.regular, size: 20)
-        self.demiBold.font = UIFont(name: Theme.demiBold, size: 20)
-        self.heavy.font = UIFont(name: Theme.heavy, size: 20)
-        self.medium.font = UIFont(name: Theme.medium, size: 20)
-        self.ultraLight.font = UIFont(name: Theme.ultraLight, size: 20)
-    }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
