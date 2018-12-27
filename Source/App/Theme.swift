18fe0bedb08eec3e7cfb9a2139939b9b27687a86
Benjamin Dodgson <benji@tomorrow.me>
2018-12-27 09:52:19 -0800

    Added theme and font names

---
+++ Source/App/Theme.swift
@@ -0,0 +1,21 @@
+//
+//  BenjiTheme.swift
+//  Benji
+//
+//  Created by Benji Dodgson on 12/25/18.
+//  Copyright © 2018 Benjamin Dodgson. All rights reserved.
+//
+
+import Foundation
+
+struct Theme {
+    static let regular: String = "AvenirNext-Regular"
+    static let demiBold: String = "AvenirNext-DemiBold"
+    static let heavy: String = "AvenirNext-Heavy"
+    static let medium: String = "AvenirNext-Medium"
+    static let ultraLight: String = "AvenirNext-UltraLight"
+
+    static let animationDuration: TimeInterval = 0.25
+
+    private init() {}
+}
