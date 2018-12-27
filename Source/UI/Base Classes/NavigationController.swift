fb4bbbe3177f1b17d80e4a3c01a7d832733b69b8
Benjamin Dodgson <benji@tomorrow.me>
2018-12-27 14:07:49 -0800

    Setting status bar style and and adding subclass for nav controllers

---
+++ Source/UI/Base Classes/NavigationController.swift
@@ -0,0 +1,13 @@
+//
+//  NavigationController.swift
+//  Benji
+//
+//  Created by Benji Dodgson on 12/27/18.
+//  Copyright © 2018 Benjamin Dodgson. All rights reserved.
+//
+
+import Foundation
+
+class NavigationController: UINavigationController {
+    
+}
--- Source/UI/Screens/Center/CenterViewController.swift
+++ Source/UI/Screens/Center/CenterViewController.swift
@@ -32,26 +32,7 @@ class CenterViewController: FullScreenViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Define the menus
-        let menuLeftNavigationController = UISideMenuNavigationController(rootViewController: self.leftVC)
-        // UISideMenuNavigationController is a subclass of UINavigationController, so do any additional configuration
-        // of it here like setting its viewControllers. If you're using storyboards, you'll want to do something like:
-        // let menuLeftNavigationController = storyboard!.instantiateViewController(withIdentifier: "LeftMenuNavigationController") as! UISideMenuNavigationController
-        SideMenuManager.default.menuLeftNavigationController = menuLeftNavigationController
-
-        let menuRightNavigationController = UISideMenuNavigationController(rootViewController: self.rightVC)
-        // UISideMenuNavigationController is a subclass of UINavigationController, so do any additional configuration
-        // of it here like setting its viewControllers. If you're using storyboards, you'll want to do something like:
-        // let menuRightNavigationController = storyboard!.instantiateViewController(withIdentifier: "RightMenuNavigationController") as! UISideMenuNavigationController
-        SideMenuManager.default.menuRightNavigationController = menuRightNavigationController
-
-        // (Optional) Enable gestures. The left and/or right menus must be set up above for these to work.
-        // Note that these continue to work on the Navigation Controller independent of the view controller it displays!
-        SideMenuManager.default.menuAddPanGestureToPresent(toView: self.navigationController!.navigationBar)
-        SideMenuManager.default.menuAddScreenEdgePanGesturesToPresent(toView: self.navigationController!.view)
-
-        // (Optional) Prevent status bar area from turning black when menu appears:
-        SideMenuManager.default.menuFadeStatusBar = false
+        self.setupSideMenu()
     }
 
     override func viewIsReadyForLayout() {
@@ -69,6 +50,47 @@ class CenterViewController: FullScreenViewController {
         self.lable2.set(attributed: string)
         self.lable3.set(attributed: string)
         self.lable4.set(attributed: string)
+
+        self.view.set(backgroundColor: .darkGray)
+    }
+
+    private func setupSideMenu() {
+        // Define the menus
+        let menuLeftNavigationController = UISideMenuNavigationController(rootViewController: self.leftVC)
+        menuLeftNavigationController.navigationBar.isHidden = true
+        menuLeftNavigationController.sideMenuDelegate = self
+        SideMenuManager.default.menuLeftNavigationController = menuLeftNavigationController
+
+        let menuRightNavigationController = UISideMenuNavigationController(rootViewController: self.rightVC)
+        menuRightNavigationController.navigationBar.isHidden = true
+        menuRightNavigationController.sideMenuDelegate = self
+        SideMenuManager.default.menuRightNavigationController = menuRightNavigationController
+
+        SideMenuManager.default.menuAddPanGestureToPresent(toView: self.navigationController!.navigationBar)
+        SideMenuManager.default.menuAddScreenEdgePanGesturesToPresent(toView: self.navigationController!.view)
+        SideMenuManager.default.menuAnimationTransformScaleFactor = 0.8
+        SideMenuManager.default.menuPresentMode = .viewSlideInOut
+
+        SideMenuManager.default.menuFadeStatusBar = false
+    }
+}
+
+extension CenterViewController: UISideMenuNavigationControllerDelegate {
+
+    func sideMenuWillAppear(menu: UISideMenuNavigationController, animated: Bool) {
+        print("SideMenu Appearing! (animated: \(animated))")
+    }
+
+    func sideMenuDidAppear(menu: UISideMenuNavigationController, animated: Bool) {
+        print("SideMenu Appeared! (animated: \(animated))")
+    }
+
+    func sideMenuWillDisappear(menu: UISideMenuNavigationController, animated: Bool) {
+        print("SideMenu Disappearing! (animated: \(animated))")
+    }
+
+    func sideMenuDidDisappear(menu: UISideMenuNavigationController, animated: Bool) {
+        print("SideMenu Disappeared! (animated: \(animated))")
     }
 }
 
