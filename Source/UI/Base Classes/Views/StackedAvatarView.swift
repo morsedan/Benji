2b6feaa16f4cda8caa2bc9a5a3e0b21743cc5f37
Benjamin Dodgson <benji@tomorrow.me>
2019-07-06 07:23:22 -0700

    Added stacked avatar view

---
+++ Source/UI/Base Classes/Views/StackedAvatarView.swift
@@ -0,0 +1,46 @@
+//
+//  StackedAvatarView.swift
+//  Benji
+//
+//  Created by Benji Dodgson on 7/6/19.
+//  Copyright © 2019 Benjamin Dodgson. All rights reserved.
+//
+
+import Foundation
+
+class StackedAvatarView: View {
+
+    private var imageViews: [AvatarView] = []
+    private let maxItems: Int = 3
+    var itemSize: CGFloat = 30
+    var offsetMultiplier: CGFloat = 0.5
+
+    func configure(items: [Avatar]) {
+
+        self.imageViews.removeAllFromSuperview(andRemoveAll: true)
+
+        let max: Int = min(items.count, self.maxItems)
+        for index in stride(from: max - 1, through: 0, by: -1) {
+            let item: Avatar = items[index]
+            let avatarView = AvatarView()
+            avatarView.set(avatar: item)
+
+            self.imageViews.append(avatarView, toSuperview: self)
+        }
+
+        self.setNeedsLayout()
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        for (index, imageView) in self.imageViews.enumerated() {
+
+            let offset = CGFloat(index) * self.itemSize * offsetMultiplier
+            imageView.size = CGSize(width: self.itemSize, height: self.itemSize)
+            imageView.right = self.width - offset
+            imageView.centerOnY()
+        }
+    }
+}
+
--- Source/UI/Screens/Channel/EmptyChannelView.swift
+++ Source/UI/Screens/Channel/EmptyChannelView.swift
@@ -14,7 +14,54 @@ class EmptyChannelView: View {
     let descriptionLabel = Label()
     let actionLabel = Label()
 
-    override func initialize() {
+    var titleText: Localized? {
+        didSet {
+            guard let text = self.titleText else { return }
 
+            let attributed = AttributedString(text,
+                                              fontType: .display1,
+                                              color: .white,
+                                              kern: 0)
+            self.titleLabel.set(attributed: attributed,
+                                alignment: .center)
+        }
+    }
+
+    var descriptionText: Localized? {
+        didSet {
+            guard let text = self.descriptionText else { return }
+
+            let attributed = AttributedString(text,
+                                              fontType: .small,
+                                              color: .white,
+                                              kern: 0)
+            self.descriptionLabel.set(attributed: attributed,
+                                      alignment: .center)
+        }
+    }
+
+    var actionText: Localized? {
+        didSet {
+            guard let text = self.actionText else { return }
+
+            let attributed = AttributedString(text,
+                                              fontType: .regular,
+                                              color: .white,
+                                              kern: 0)
+            self.actionLabel.set(attributed: attributed,
+                                 alignment: .center)
+        }
+    }
+
+    override func initialize() {
+        self.addSubview(self.titleLabel)
+        self.addSubview(self.descriptionLabel)
+        self.addSubview(self.actionLabel)
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        self.tt
     }
 }
