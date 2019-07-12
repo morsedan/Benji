d215ca30cf762bd67f910de94e1a354f55823e72
Benjamin Dodgson <benji@tomorrow.me>
2019-07-11 20:34:55 -0700

    Added cellsizecalculator class

---
+++ Source/UI/Screens/New/CellSizeCalculator.swift
@@ -0,0 +1,33 @@
+//
+//  CellSizeCalculator.swift
+//  Benji
+//
+//  Created by Benji Dodgson on 7/11/19.
+//  Copyright © 2019 Benjamin Dodgson. All rights reserved.
+//
+
+import Foundation
+
+/// An object is responsible for
+/// sizing and configuring cells for given `IndexPath`s.
+class CellSizeCalculator {
+
+    /// The layout object for which the cell size calculator is used.
+    weak var layout: UICollectionViewFlowLayout?
+
+    /// Used to configure the layout attributes for a given cell.
+    ///
+    /// - Parameters:
+    /// - attributes: The attributes of the cell.
+    /// The default does nothing
+    func configure(attributes: UICollectionViewLayoutAttributes) {}
+
+    /// Used to size an item at a given `IndexPath`.
+    ///
+    /// - Parameters:
+    /// - indexPath: The `IndexPath` of the item to be displayed.
+    /// The default return .zero
+    func sizeForItem(at indexPath: IndexPath) -> CGSize { return .zero }
+
+    init() {}
+}
--- Source/UI/Screens/New/ChannelCollectionViewFlowLayout.swift
+++ Source/UI/Screens/New/ChannelCollectionViewFlowLayout.swift
@@ -9,5 +9,74 @@
 import Foundation
 
 class ChannelCollectionViewFlowLayout: UICollectionViewFlowLayout {
-    
+
+    var channelCollectionView: ChannelCollectionView {
+        guard let channelCollectionView = collectionView as? ChannelCollectionView else {
+            fatalError("ChannelCollectionView NOT FOUND")
+        }
+        return channelCollectionView
+    }
+
+    var dataSource: ChannelDataSource {
+        guard let dataSource = self.channelCollectionView.channelDataSource else {
+            fatalError("ChannelDataSource is nil")
+        }
+        return dataSource
+    }
+
+    var itemWidth: CGFloat {
+        guard let collectionView = self.collectionView else { return 0 }
+        return collectionView.frame.width - self.sectionInset.left - self.sectionInset.right
+    }
+
+    override init() {
+        super.init()
+        self.setupView()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.setupView()
+    }
+
+    private func setupView() {
+        self.sectionInset = UIEdgeInsets(top: 4, left: 8, bottom: 4, right: 8)
+    }
+
+    // MARK: - Attributes
+
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        guard let attributesArray = super.layoutAttributesForElements(in: rect) as? [ChannelCollectionViewLayoutAttributes] else {
+            return nil
+        }
+        for attributes in attributesArray where attributes.representedElementCategory == .cell {
+           // let cellSizeCalculator = cellSizeCalculatorForItem(at: attributes.indexPath)
+           // cellSizeCalculator.configure(attributes: attributes)
+        }
+        return attributesArray
+    }
+
+    override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        guard let attributes = super.layoutAttributesForItem(at: indexPath) as? ChannelCollectionViewLayoutAttributes else {
+            return nil
+        }
+        if attributes.representedElementCategory == .cell {
+            //let cellSizeCalculator = cellSizeCalculatorForItem(at: attributes.indexPath)
+            //cellSizeCalculator.configure(attributes: attributes)
+        }
+        return attributes
+    }
+
+    // MARK: - Layout Invalidation
+
+    override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
+        return collectionView?.bounds.width != newBounds.width
+    }
+
+    override func invalidationContext(forBoundsChange newBounds: CGRect) -> UICollectionViewLayoutInvalidationContext {
+        let context = super.invalidationContext(forBoundsChange: newBounds)
+        guard let flowLayoutContext = context as? UICollectionViewFlowLayoutInvalidationContext else { return context }
+        flowLayoutContext.invalidateFlowLayoutDelegateMetrics = self.shouldInvalidateLayout(forBoundsChange: newBounds)
+        return flowLayoutContext
+    }
 }
