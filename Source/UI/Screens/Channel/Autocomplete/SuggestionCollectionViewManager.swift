72af273f43801ac75630bbcd7dc18bd72ee0255a
Benjamin Dodgson <benji@tomorrow.me>
2019-10-22 19:34:41 -0700

    adding data source to manager

--- Source/UI/Screens/Channel/Autocomplete/SuggestionCollectionViewManager.swift
+++ Source/UI/Screens/Channel/Autocomplete/SuggestionCollectionViewManager.swift
@@ -13,9 +13,31 @@ class SuggestionCollectionViewManager: CollectionViewManager<SuggestionCell> {
     /// holds keyboard appearance state for current textfield.
     var keyboardAppearance: UIKeyboardAppearance = .light
 
+    override func initializeCollectionView() {
+        super.initializeCollectionView()
+
+        self.updateLayoutDataSource()
+    }
+
+    private func updateLayoutDataSource() {
+        if let layout = self.collectionView.collectionViewLayout as? SuggestionCollectionViewFlowLayout {
+            layout.dataSource = self
+        }
+    }
+
     override func managerWillDisplay(cell: SuggestionCell, for indexPath: IndexPath) {
         super.managerWillDisplay(cell: cell, for: indexPath)
 
         cell.backgroundStyle = self.keyboardAppearance
+    }
+
+    override func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+        guard let layout = collectionViewLayout as? SuggestionCollectionViewFlowLayout,
+            let suggestion = self.items.value[safe: indexPath.row] else { return .zero }
+
+        return layout.sizeForItem(at: indexPath, with: suggestion)
     }
 }
