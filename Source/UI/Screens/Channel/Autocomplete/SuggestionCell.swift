e3cbce4b009f7eecd25ef8332dbfde66d00d3c91
Benjamin Dodgson <benji@tomorrow.me>
2019-10-28 20:33:31 -0700

    adding label to suggestion cell

--- Source/UI/Screens/Channel/Autocomplete/SuggestionCell.swift
+++ Source/UI/Screens/Channel/Autocomplete/SuggestionCell.swift
@@ -13,28 +13,38 @@ class SuggestionCell: UICollectionViewCell, DisplayableCell {
 
     var backgroundStyle: UIKeyboardAppearance = .light
 
-    func configure(with item: SuggestionType?) {
+    let label = RegularSemiBoldLabel()
 
+    func configure(with item: SuggestionType?) {
+        guard let suggestion = item else { return }
+
+        switch suggestion {
+        case .text(let text):
+            self.label.set(text: text)
+        }
+
+        self.layoutNow()
     }
 
     override func layoutSubviews() {
         super.layoutSubviews()
 
-
+        self.label.setSize(withWidth: self.contentView.width * 0.8)
+        self.label.centerOnXAndY()
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-         super.touchesBegan(touches, with: event)
+        super.touchesBegan(touches, with: event)
         self.contentView.scaleDown()
-     }
+    }
 
-     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-         super.touchesEnded(touches, with: event)
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
         self.contentView.scaleUp()
-     }
+    }
 
-     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-         super.touchesCancelled(touches, with: event)
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesCancelled(touches, with: event)
         self.contentView.scaleUp()
-     }
+    }
 }
