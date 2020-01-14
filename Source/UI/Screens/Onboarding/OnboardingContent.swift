69260ae71ba34ad7e00b81b646319c55ebb77193
Benjamin Dodgson <benji@tomorrow.me>
2020-01-14 10:27:30 -0800

    Adding switch content function

---
+++ Source/UI/Screens/Onboarding/OnboardingContent.swift
@@ -0,0 +1,47 @@
+//
+//  OnboardingContent.swift
+//  Benji
+//
+//  Created by Benji Dodgson on 1/14/20.
+//  Copyright © 2020 Benjamin Dodgson. All rights reserved.
+//
+
+import Foundation
+
+enum OnboardingContent: Equatable {
+    case reservation(ReservationViewController)
+    case phone(LoginPhoneViewController)
+    case code(LoginCodeViewController)
+    case name(LoginNameViewController)
+    case photo(LoginProfilePhotoViewController)
+
+    var vc: UIViewController {
+        switch self {
+        case .reservation(let vc):
+            return vc
+        case .phone(let vc):
+            return vc
+        case .code(let vc):
+            return vc
+        case .name(let vc):
+            return vc
+        case .photo(let vc):
+            return vc
+        }
+    }
+
+    var showBackButton: Bool {
+        switch self {
+        case .reservation(_):
+            return false
+        case .phone(_):
+            return true
+        case .code(_):
+            return true
+        case .name(_):
+            return false
+        case .photo(_):
+            return true
+        }
+    }
+}
--- Source/UI/Screens/Onboarding/OnboardingViewController.swift
+++ Source/UI/Screens/Onboarding/OnboardingViewController.swift
@@ -9,6 +9,7 @@
 import Foundation
 import PhoneNumberKit
 import Parse
+import ReactiveSwift
 
 protocol OnboardingViewControllerDelegate: class {
     func onboardingView(_ controller: OnboardingViewController, didVerify user: PFUser)
@@ -22,6 +23,9 @@ class OnboardingViewController: NavigationBarViewController {
     lazy var nameVC = LoginNameViewController(with: self)
     lazy var photoVC = LoginProfilePhotoViewController(with: self)
 
+    lazy var currentContent = MutableProperty<OnboardingContent>(.reservation(self.reservationVC))
+    private var currentCenterVC: UIViewController?
+    
     unowned let delegate: OnboardingViewControllerDelegate
 
     init(with delegate: OnboardingViewControllerDelegate) {
@@ -33,6 +37,55 @@ class OnboardingViewController: NavigationBarViewController {
         fatalError("init(coder:) has not been implemented")
     }
 
+    override func initializeViews() {
+        super.initializeViews()
+
+        self.currentContent.producer
+            .skipRepeats()
+            .on { [unowned self] (contentType) in
+                self.switchContent()
+        }.start()
+    }
+
+    private func switchContent() {
+
+        UIView.animate(withDuration: Theme.animationDuration, animations: {
+            self.titleLabel.alpha = 0
+            self.titleLabel.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+
+            self.descriptionLabel.alpha = 0
+            self.descriptionLabel.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+
+            self.currentCenterVC?.view.alpha = 0
+            self.backButton.alpha = 0
+
+        }) { (completed) in
+
+            self.currentCenterVC?.removeFromParentSuperview()
+
+            self.updateLabels()
+
+            self.currentCenterVC = self.currentContent.value.vc
+            let showBackButton = self.currentContent.value.showBackButton
+
+            if let contentVC = self.currentCenterVC {
+                self.addChild(viewController: contentVC, toView: self.scrollView)
+            }
+
+            self.view.setNeedsLayout()
+
+            UIView.animate(withDuration: Theme.animationDuration) {
+                self.titleLabel.alpha = 1
+                self.titleLabel.transform = .identity
+
+                self.descriptionLabel.alpha = 1
+                self.descriptionLabel.transform = .identity
+
+                self.currentCenterVC?.view.alpha = 1
+                self.backButton.alpha = showBackButton ? 1 : 0
+            }
+        }
+    }
 }
 
 extension OnboardingViewController: LoginPhoneViewControllerDelegate {
