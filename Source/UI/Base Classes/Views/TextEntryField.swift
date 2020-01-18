db0b397eda4d5cdb59350818aef4d85a65f654a2
Benjamin Dodgson <benji@tomorrow.me>
2020-01-18 09:59:47 -0800

    Adding textField entry class

---
+++ Source/UI/Base Classes/Views/TextEntryField.swift
@@ -0,0 +1,50 @@
+//
+//  TextEntryField.swift
+//  Benji
+//
+//  Created by Benji Dodgson on 1/18/20.
+//  Copyright © 2020 Benjamin Dodgson. All rights reserved.
+//
+
+import Foundation
+import TMROLocalization
+
+class TextEntryField: View, Sizeable {
+
+    private let titleLabel = RegularBoldLabel()
+    private let textField: UITextField
+    private let title: Localized
+
+    init(with textField: UITextField, title: Localized) {
+        self.textField = textField
+        self.title = title
+        super.init()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func initializeSubviews() {
+        super.initializeSubviews()
+
+        self.addSubview(self.titleLabel)
+        self.titleLabel.set(text: self.title, stringCasing: .unchanged)
+        self.addSubview(self.textField)
+        self.textField.set(backgroundColor: .background3)
+        self.textField.roundCorners()
+    }
+
+    func getHeight(for width: CGFloat) -> CGFloat {
+
+        self.titleLabel.setSize(withWidth: width)
+        self.titleLabel.top = 0
+        self.titleLabel.left = 0
+
+        self.textField.size = CGSize(width: width, height: 40)
+        self.textField.left = 0
+        self.textField.top = self.titleLabel.bottom + 10
+
+        return self.textField.bottom
+    }
+}
